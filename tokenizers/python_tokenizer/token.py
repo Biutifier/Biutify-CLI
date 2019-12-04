ac3b36a424c8ffad68e0e3a9666421710f1cf574
ZachDaChampion <zachchampion79@gmail.com>
2019-12-04 08:58:24 -0500

    look idk im still figure this out

--- tokenizers/python_tokenizer/token.py
+++ tokenizers/python_tokenizer/token.py
@@ -1,6 +1,24 @@
+from enum import Enum
+
+class TokenType(Enum):
+
+    NONE = 0
+    NEWLINE = 1
+    INDENT = 2
+    DEDENT = 3
+    IDENTIFIER = 4
+    KEYWORD = 5
+    LITERAL = 6
+    OPERATOR = 7
+    DELIMITER = 8
+
+
 class Token:
 
-    pass
+    token_type = TokenType.NONE
+
+
+# https://docs.python.org/3/reference/lexical_analysis.html
 
 
 '''

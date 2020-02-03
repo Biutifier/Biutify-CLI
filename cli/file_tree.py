3c6aeae189a88aa08bae5c6092d1d223100db98a
ZachDaChampion <zachchampion79@gmail.com>
2020-02-03 08:49:59 -0500

    begin Node and FileTree classes

---
+++ cli/file_tree.py
@@ -0,0 +1,8 @@
+class FileTree:
+  """
+  A tree used to parse and regenerate files.
+
+  Attributes
+  ----------
+  root : Node
+    The node at the root of the tree.
---
+++ cli/node.py
@@ -0,0 +1,17 @@
+class Node:
+  """
+  A node used to build a file tree.
+
+  Attributes
+  ----------
+  children : list of Nodes
+    A list of this node's child nodes.
+  parent : Node
+    A reference to the parent of this node.
+
+  Methods
+  -------
+
+  """
+
+  
